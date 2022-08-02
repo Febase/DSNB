b0ecd80a143b4b2e8af5b2392b4b0c7258f95839
SY-MOON <seokyoung.moon90@gmail.com>
2022-08-02 23:11:56 +0900

    💫 종이 전환 애니메이션 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,21 @@
-import { useState } from 'react'
-import logo from './logo.svg'
+import { useState } from 'react';
 import './App.css'
+import DSNBBase from './components/DSNBBase'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isStart, setStart] = useState<boolean>(false);
+
+  const handleStart = () => {
+    setStart(true);
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <DSNBBase isStart={isStart} />
+      {/* TODO: 결과 나온뒤 버튼 활성화 처리. 스타일추가 */}
+      {!isStart && <button onClick={handleStart} disabled={isStart}> 
+        START
+      </button>}
     </div>
   )
 }
